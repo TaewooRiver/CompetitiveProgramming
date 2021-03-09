4e7a1d82b6bd4311f8bd492fe0a1391167a23c9e
Yun_on_a_Journey <60255486+TaewooRiver@users.noreply.github.com>
2021-03-09 21:12:57 +0900

    Update Test.cpp

--- USACO/Test.cpp
+++ USACO/Test.cpp
@@ -1,11 +1,9 @@
-/* Use the slash-star style comments or the system won't see your
-   identification information */
+
 /*
 ID: cooltwk1
 TASK: test
 LANG: C++                 
 */
-/* LANG can be C++11 or C++14 for those more recent releases */
 #include <iostream>
 #include <fstream>
 #include <string>
