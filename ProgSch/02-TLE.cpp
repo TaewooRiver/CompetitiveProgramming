b5565416b97a0800845dc11a9492721723caae79
Taewoo Kang <cooltwk@naver.com>
2021-08-14 05:34:24 +0000

    ProgrammersSchool

---
+++ ProgSch/02-TLE.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool solution(vector<int> arr)
+{
+    bool answer = true;
+
+    for(int i=1;i<arr.size()+1;++i){
+        int numOccured = 0;
+        
+        for(int j=1;j<arr.size()+1;++j){
+            if(i==arr.at(j-1)) numOccured++;
+            if (arr.at(j-1) > arr.size()) return false;
+        }
+        if(numOccured != 1) return false;
+    }
+
+    return answer;
+}
